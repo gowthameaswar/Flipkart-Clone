981ba130d9cd696b66c47bc7ccaa919c278d25e3
gowthameaswar <82211443+gowthameaswar@users.noreply.github.com>
2022-10-30 21:14:43 +0530

    Added SQL files

--- ecom.sql
+++ ecom.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.0
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 20, 2021 at 02:48 PM
--- Server version: 10.4.19-MariaDB
--- PHP Version: 8.0.6
+-- Generation Time: Oct 30, 2022 at 04:21 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 7.4.23
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -38,7 +38,7 @@ CREATE TABLE `admin_users` (
 --
 
 INSERT INTO `admin_users` (`id`, `username`, `password`) VALUES
-(1, 'admin', 'admin'),
+(1, 'Gowtham', 'Gowtham'),
 (2, 'dharun', 'dharun');
 
 -- --------------------------------------------------------
@@ -79,6 +79,7 @@ CREATE TABLE `product` (
 --
 
 INSERT INTO `product` (`id`, `categories_id`, `name`, `image`, `price`) VALUES
+(0, 2, 'Samsung fridge', '', 9000),
 (1, 1, 'Vivo', 'mobile1.jpg', 15000),
 (2, 1, 'Oppo', 'mobile2.jpg', 18000),
 (3, 1, 'IPhone', 'mobile3.jpg', 45000),
@@ -104,7 +105,7 @@ CREATE TABLE `usertable` (
 --
 
 INSERT INTO `usertable` (`id`, `name`, `email`, `password`, `code`, `status`) VALUES
-(11, 'Gowtham easwar', 'gowthameaswar344@gmail.com', '$2y$10$5gcsRd/DHQiT3eqbnnSaCuyml7uzVIrqgpKiH4VZPpG2z5HU/i.Z6', 0, 'verified');
+(15, 'Gowtham Easwar B', 'gowthambalaji344@gmail.com', '$2y$10$H8.LZm58914FWXYKkym8nedZt.3K7XR0n/FlHBd4IhCiEhOfVaB.C', 953305, 'verified');
 
 --
 -- Indexes for dumped tables
@@ -154,7 +155,7 @@ ALTER TABLE `categories`
 -- AUTO_INCREMENT for table `usertable`
 --
 ALTER TABLE `usertable`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
